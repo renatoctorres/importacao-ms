b2a5ef8ee98ef5b9dec3c5e2ae1a9ec5814301bb
renatoctorres <renatoctorres@gmail.com>
2020-11-08 16:56:33 -0300

    entrega inicial

---
+++ src/main/java/com/empresa/importacao/ImportacaoMsApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.importacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportacaoMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportacaoMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empresa/importacao/ImportacaoMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.importacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImportacaoMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
